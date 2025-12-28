22d761bb5440bb5a56890bb55ec5ebee6d6841d1
Lucía Martínez <lucia.martinez@gracia.lasalle.cat>
2025-12-28 18:04:22 +0100

    chore: inicializar variables de usuraio para valores por defecto

--- app/src/main/java/com/example/loginresponsiveadaptative/model/User.kt
+++ app/src/main/java/com/example/loginresponsiveadaptative/model/User.kt
@@ -1,12 +1,12 @@
 package com.example.loginresponsiveadaptative.model
 
 data class User(
-    var name: String,
-    var birth: String,
-    var email: String,
-    var phone: Int,
-    val username: String,
-    var password: String,
-    var confirmPassword: String,
-    var conditions: Boolean
+    var name: String = "",
+    var birth: String = "",
+    var email: String = "",
+    var phone: Int = 0,
+    var username: String = "",
+    var password: String = "",
+    var confirmPassword: String = "",
+    var conditions: Boolean = false
 )
